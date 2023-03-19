318dd149e96868c592e5425ea756c67fb1eef10a
Eliel10268 <ndaeliel123@gmail.com>
2023-03-19 04:10:36 +0000

    Python Task 01 from Hyperiondev

---
+++ hello_world.py
@@ -0,0 +1,14 @@
+
+
+#this message will be displayed on the screen asking the user's name
+name = input("Enter your name here: ")
+#this message will be displayed on the screen asking the user's age
+age = input("Enter your age here: ")
+
+#user's name will be displayed
+print("Your name is : "+name)
+#user's age will be displayed
+print("Your age is : "+age)
+
+#hello world message will be displayed on the screen
+print("Hello World!")
---
+++ menu.py
@@ -0,0 +1,14 @@
+
+#this message will be displayed on the screen to ask the user their favourite food items
+print("Please Enter your 3 favorites food on menu: ")
+#this will hold the users favourite items
+item1 = input()
+item2 = input()
+item3 = input()
+
+#ask the user what their confirmation is
+print("Order confirmation! You have ordered: ")
+#this message will print on the screen the client orders
+print(item1)
+print(item2)
+print(item3)
